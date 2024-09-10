f98a022a7ac2fa936569db692c5ceb1b640a3ece
Aritra Chakraborty <107548404+AritraChakraborty2003@users.noreply.github.com>
2024-09-10 21:57:53 +0530

    Update:updated the UI for logo making it the home link

--- Client/src/Components/Header.jsx
+++ Client/src/Components/Header.jsx
@@ -15,10 +15,12 @@ const Header = (props) => {
         <div className="header bg-[rgb(232,111,63)] text-white w-[100vw] lg:w-[100vw] flex pt-5 border-[2px]">
           <div className="logo h-[10vmin]">
             <div className="imageHolder h-[10vmin] w-[25vmin] ml-4 mt-[-2vmin] lg:mt-[-0.5vmin]">
-              <img
-                src="https://i.postimg.cc/PxsrXgMy/iic-removebg-preview-1.png"
-                className="max-w-full max-h-full object-contain"
-              />
+              <a href="/">
+                <img
+                  src="https://i.postimg.cc/PxsrXgMy/iic-removebg-preview-1.png"
+                  className="max-w-full max-h-full object-contain"
+                />
+              </a>
             </div>
           </div>
           <div className="middleHolder w-[50vmin] lg:w-[25vw]"></div>
